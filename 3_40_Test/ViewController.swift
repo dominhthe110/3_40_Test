01bf57710a89d4edfa0016bde53a44a0c2c4cf2d
The M. Do <dominhthe110@gmail.com>
2017-08-25 14:39:29 +0700

    Initial Commit

---
+++ 3_40_Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  3_40_Test
+//
+//  Created by Minh The on 8/25/17.
+//  Copyright © 2017 Minh The. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
